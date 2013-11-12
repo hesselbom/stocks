1403052bdea8cca07f690c8a105683f6ad853841
viktor hesselbom <viktor@hesselbom.net>
2013-11-12 13:27:14 +0100

    Updated to use tabulate and colors

--- stocks.py
+++ stocks.py
@@ -8,67 +8,7 @@ import os
 
 from time import time, sleep, strftime
 from operator import itemgetter
-
-# Function from http://www.calazan.com/python-function-for-displaying-a-list-of-dictionaries-in-table-format/
-def format_as_table(data,
-                    keys,
-                    header=None,
-                    sort_by_key=None,
-                    sort_order_reverse=False):
-    """Takes a list of dictionaries, formats the data, and returns
-    the formatted data as a text table.
-
-    Required Parameters:
-        data - Data to process (list of dictionaries). (Type: List)
-        keys - List of keys in the dictionary. (Type: List)
-
-    Optional Parameters:
-        header - The table header. (Type: List)
-        sort_by_key - The key to sort by. (Type: String)
-        sort_order_reverse - Default sort order is ascending, if
-            True sort order will change to descending. (Type: Boolean)
-    """
-    # Sort the data if a sort key is specified (default sort order
-    # is ascending)
-    if sort_by_key:
-        data = sorted(data,
-                      key=itemgetter(sort_by_key),
-                      reverse=sort_order_reverse)
-
-    # If header is not empty, add header to data
-    if header:
-        # Get the length of each header and create a divider based
-        # on that length
-        header_divider = []
-        for name in header:
-            header_divider.append('-' * len(name))
-
-        # Create a list of dictionary from the keys and the header and
-        # insert it at the beginning of the list. Do the same for the
-        # divider and insert below the header.
-        header_divider = dict(zip(keys, header_divider))
-        data.insert(0, header_divider)
-        header = dict(zip(keys, header))
-        data.insert(0, header)
-
-    column_widths = []
-    for key in keys:
-        column_widths.append(max(len(str(column[key])) for column in data))
-
-    # Create a tuple pair of key and the associated column width for it
-    key_width_pair = zip(keys, column_widths)
-
-    format = ('%-*s  ' * len(keys)).strip() + '\n'
-    formatted_data = ''
-    for element in data:
-        data_to_format = []
-        # Create a tuple that will be used for the formatting in
-        # width, value format
-        for pair in key_width_pair:
-            data_to_format.append(pair[1])
-            data_to_format.append(element[pair[0]])
-        formatted_data += format % tuple(data_to_format)
-    return formatted_data
+from tabulate import tabulate
 
 # Fetch stock quote
 def get_stock_quote(ticker_symbol):
@@ -76,18 +16,34 @@ def get_stock_quote(ticker_symbol):
     lines = urllib2.urlopen(url).read().splitlines()
     return json.loads(''.join([x for x in lines if x not in ('// [', ']')]))
 
+def format_quote(ticker):
+    ticker['c'] = format_number(ticker['c'])
+    ticker['cp'] = format_number(ticker['cp'])
+    return ticker
+
+def format_number(number):
+    if float(number) > 0:
+        return '\033[92m' + number + '\033[0m'
+    elif float(number) < 0:
+        return '\033[31m' + number + '\033[0m'
+    else:
+        return '\033[31m' + number + '\033[0m'
+
 def update(lines):
     data = []
 
-    for ticker in lines:
-        data.append( get_stock_quote(ticker) )
-
     header = ['Exchange', 'Ticker', 'Current', 'Change', 'Change %', 'Latest Trade']
-    keys = ['e', 't', 'l_cur', 'c', 'cp', 'lt']
+    keys = ['e', 't', 'l', 'c', 'cp', 'lt']
     sort_by_key = 't'
     sort_order_reverse = True
 
-    return '\033[1;31mUpdated at: %s\033[0m\n%s' % (strftime("%H:%M:%S"), format_as_table(data, keys, header, sort_by_key, sort_order_reverse))
+    x = [[]]
+
+    for ticker in lines:
+        quote = format_quote(get_stock_quote(ticker))
+        data.append( [quote['e'], quote['t'], quote['l'], quote['c'], quote['cp'], quote['lt']] )
+
+    return '\033[1;31mUpdated at: %s\033[0m\n%s' % (strftime("%H:%M:%S"), tabulate(data, header, tablefmt="orgtbl"))
 
 if __name__ == '__main__':
     # Fetch tickers from relative 'stocks' file
